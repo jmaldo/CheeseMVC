4ed607610323b37a4f837ab3c80133aaa9572759
Jeremy Maldonado <jeremymaldonado99@hotmail.com>
2018-02-18 23:29:09 -0600

    Opened wrong lesson. Converted Back

--- src/CheeseMVC/Models/Cheese.cs
+++ src/CheeseMVC/Models/Cheese.cs
@@ -9,6 +9,7 @@ namespace CheeseMVC.Models
     {
         public string Name { get; set; }
         public string Description { get; set; }
+
         public int CheeseId { get; set; }
         private static int nextId = 1;
 
